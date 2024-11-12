35d97456ffffd3e27ecd2aeb9a6bddb16965452d
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-12 12:21:30 -0300

    feat: template footer

--- public/views/layouts/footer.php
+++ public/views/layouts/footer.php
@@ -1,5 +1,59 @@
         <footer class="footer">
-            <p>&copy; 2024 - Seu Nome ou Empresa</p>
+            <div class="footer__informations">
+                <figure class="footer__logo">
+                    <img src="/assets/images/logo.webp" alt="Logo - Prados Turismo">
+                </figure>
+                <div class="footer__sac">
+                    <h1 class="footer__title">Atendimento</h1>
+                    <div class="footer__content">
+                        <h2>Horario de Atendimento</h2>
+                        <p>Segunda a sexta 8:00 ás 23:00h</p>
+                        <p>+55 (85) 9 9746-0786</p>
+                        <p>Fortaleza, Ceará</p>
+                        <p><a href="mailto:contato@pradosturismo.com.br">contato@pradosturismo.com.br</a></p>
+                    </div>
+                </div>
+                <div class="footer__sitemap">
+                    <h1 class="footer__title">Ajuda</h1>
+                    <ul class="footer__list">
+                        <li><strong>31.853.548 - Emannuel Wallysson Aguiar do Prado.</strong></li>
+                        <li>
+                            <a href="">Sobre nós</a>
+                        </li>
+                        <li>
+                            <a href="">Politica de privacidade</a>
+                        </li>
+                        <li>
+                            <a href="">Termos de uso</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__others">
+                    <div class="footer__payments-method">
+                        <h1 class="footer__title">Formas de Pagamento</h1>
+                    </div>
+                    <div class="footer__social-media">
+                        <h1 class="footer__title">Redes sociais</h1>
+                        <ul class="footer__social-icons">
+                            <li class="footer__icon">
+                                <i class="fa-brands fa-facebook"></i>
+                            </li>
+                            <li class="footer__icon">
+                                <i class="fa-brands fa-instagram"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__copyright">
+                <div class="container">
+                    <div class="d-flex flex-column align-items-center">
+                        <strong>Prados © <?php echo date("Y"); ?> Todos os direitos reservados.</strong>
+                        <span>CNPJ: 31.853.548/0001-08</span>
+                    </div>
+                </div>
+            </div>
         </footer>
-    </body>
-</html>
+        </body>
+
+        </html>
