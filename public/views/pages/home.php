414381c0f7735eb228a812869433390e0db35c47
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-13 20:06:00 -0300

    fix: layout card adjusted

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -27,22 +27,60 @@
         <div class="container">
             <h3 class="home__title">Nossos Pacotes</h3>
             <div class="home__events">
-                <div class="card-event">
-                    <div class="card-event__content">
-                        <figure class="card-event__thumbnail">
-                            <img src="/assets/images/rj.png" alt="Rio de Janeiro">
-                        </figure>
-                        <span class="card-event__category">Fortaleza</span>
-                        <h2 class="card-event__title">Pacote para Rio de Janeiro e Arraial do Cabo</h2>
-                        <p class="card-event__resume">Conheça um dos destinos mais desejados do mundo inteiro! Neste pacote, você terá suporte 24 horas da nossa equipe, tanto presencial quanto online, transportes confortáveis...</p>
+                <?php
+                    $pacotes = [
+                        1 => [
+                            'id' => 1,
+                            'nome' => 'Pacote para Fortaleza',
+                            'descricao' => 'Uma viagem incrível para Fortaleza.',
+                            'preco' => 'R$ 1500,00',
+                            'duracao' => '7 dias e 6 noites',
+                            'imagem' => '/assets/images/rj.png',
+                            'category' => 'Fortaleza'
+                        ],
+                        2 => [
+                            'id' => 2,
+                            'nome' => 'Pacote para Salvador',
+                            'descricao' => 'Explore as maravilhas de Salvador.',
+                            'preco' => 'R$ 1800,00',
+                            'duracao' => '5 dias e 4 noites',
+                            'imagem' => '/assets/images/rj.png',
+                            'category' => 'Fortaleza'
+
+                        ],
+                        3 => [
+                            'id' => 3,
+                            'nome' => 'Pacote para Salvador',
+                            'descricao' => 'Explore as maravilhas de Salvador.',
+                            'preco' => 'R$ 1800,00',
+                            'duracao' => '5 dias e 4 noites',
+                            'imagem' => '/assets/images/rj.png',
+                            'category' => 'Fortaleza'
+                        ]
+                        // Adicione mais pacotes conforme necessário
+                    ];
+                    
+                    foreach ($pacotes as $key => $value) : ?>
+                    <div class="card-event">
+                        <div class="card-event__content">
+                            <figure class="card-event__thumbnail">
+                                <img src="<?php echo $value['imagem']; ?>" alt="Rio de Janeiro">
+                                <span class="card-event__date">
+                                    <?php echo $value['duracao']; ?>
+                                </span>
+                            </figure>
+                            <span class="card-event__category"><?php echo $value['category']; ?></span>
+                            <h2 class="card-event__title"><?php echo $value['nome']; ?></h2>
+                            <p class="card-event__resume"><?php echo $value['descricao']; ?></p>
+                        </div>
+                        <div class="card-event__actions">
+                            <span class="card-event__price"><?php echo $value['preco']; ?></span>
+                            <button class="card-event__cart">
+                                <i class="fa-solid fa-basket-shopping"></i>
+                            </button>
+                        </div>
                     </div>
-                    <div class="card-event__actions">
-                        <span class="card-event__price">R$ 2.990,00</span>
-                        <button class="card-event__cart">
-                            <i class="fa-solid fa-basket-shopping"></i>
-                        </button>
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
