63fc2bf5639b88dae440593b8b6f07f4780f1824
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-20 13:36:04 -0300

    feat: created 404 page

---
+++ public/views/pages/404.php
@@ -0,0 +1,9 @@
+<main>
+    <div class="container">
+        <div class="d-flex flex-column align-items-center">
+            <h1 class="display-1 text-center">404 Not Found</h1>
+            <p class="lead text-center">A página que você está procurando não foi encontrada.</p>
+            <button onClick="window.location.href='home';" href="home" class="btn btn-primary">Voltar para a Home</button>
+        </div>
+    </div>
+</main>
