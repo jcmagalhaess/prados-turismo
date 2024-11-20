8dc785a67a7a38f07fbcaa9c4f81424510bcdbfa
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-20 14:10:42 -0300

    wip: panel page in building

--- public/views/pages/minha-conta.php
+++ public/views/pages/minha-conta.php
@@ -2,6 +2,28 @@
     <div class="container">
         <div class="d-flex">
             <?php include "./views/layouts/dashboard-menu.php"; ?>
+
+            <div class="login__box w-75">
+                <div class="my-account__panel">
+                    <div class="login__box w-50">
+                        <div class="my-account__avatar">
+                            <figure class="my-account__image">
+                                <img src="./assets/images/avatar.png" alt="Avatar">
+                            </figure>
+                            <h5>Bem vindo, Júlio Magalhães</h5>
+                            <p>jcmagalhaes301@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <h5 class="my-account__title">Resumo do seu último pedido</h5>
+                    <h5 class="my-account__title">Atalhos</h5>
+                    <div class="my-account__shortcuts">
+                        <a href="http://">Meus Pedidos</a>
+                        <a href="http://">Pacotes</a>
+                        <a href="http://">Carrinho</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
