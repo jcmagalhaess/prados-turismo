92cf409a6d3c64680eb629df5ee1e1ae4dfdd967
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-24 14:03:53 -0300

    feat: order resume in the panel

--- public/views/pages/minha-conta.php
+++ public/views/pages/minha-conta.php
@@ -16,7 +16,42 @@
                     </div>
 
                     <h5 class="my-account__title">Resumo do seu último pedido</h5>
+
+                    <div class="my-account__resume">
+                        <div class="my-account__order-infos">
+                            <div class="my-account__item">
+                                <span>Pedido realizado</span>
+                                <span>07/11/2024 11:51:12</span>
+                            </div>
+                            <div class="my-account__item">
+                                <span>Total</span>
+                                <span>R$ 250,00</span>
+                            </div>
+                            <div class="spacer"></div>
+                            <div class="my-account__item">
+                                <span>Pedido #123456</span>
+                            </div>
+                        </div>
+                        <div class="my-account__order-content">
+                            <div class="my-account__thumb">
+                                <img src="https://mediumpurple-turtle-783032.hostingersite.com/wp-content/uploads/2024/08/17.png" alt="Natal, Pipa e Perobas"> 
+                            </div>
+                            <div class="my-account__event">
+                                <div class="my-account__name">
+                                    <strong>Evento: </strong> Natal, Pipa e Perobas
+                                </div>
+                                <div class="my-account__amout">
+                                    <strong>Quantidade de participantes: </strong> 2
+                                </div>
+                                <div class="my-account__date">
+                                    <strong>Data do evento:</strong> 01/11/2024 00:00:00 à 31/03/2025 23:59:59
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                     <h5 class="my-account__title">Atalhos</h5>
+
                     <div class="my-account__shortcuts">
                         <a href="http://">Meus Pedidos</a>
                         <a href="http://">Pacotes</a>
