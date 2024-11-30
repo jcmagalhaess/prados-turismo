1c4653d801d8936831b014cbf9ea1b6e1cecdcbf
Júlio Magalhães <jcmagalhaes301@gmail.com>
2024-11-30 19:20:21 -0300

    fix: ajuste de css no card-event

--- views/pages/home.php
+++ views/pages/home.php
@@ -23,10 +23,10 @@
         </div>
     </section>
 
-    <section class="home__pacotes">
+    <section class="home__pacotes mb-5">
         <div class="container">
             <h3 class="home__title">Nossos Pacotes</h3>
-            <div class="home__events">
+            <div class="row">
                 <?php
                     $pacotes = [
                         1 => [
@@ -61,23 +61,25 @@
                     ];
                     
                     foreach ($pacotes as $key => $value) : ?>
-                    <div class="card-event">
-                        <div class="card-event__content">
-                            <figure class="card-event__thumbnail">
-                                <img src="<?php echo getAbsoluteUrl($value['imagem']); ?>" alt="Rio de Janeiro">
-                                <span class="card-event__date">
-                                    <?php echo $value['duracao']; ?>
-                                </span>
-                            </figure>
-                            <span class="card-event__category"><?php echo $value['category']; ?></span>
-                            <h2 class="card-event__title"><?php echo $value['nome']; ?></h2>
-                            <p class="card-event__resume"><?php echo $value['descricao']; ?></p>
-                        </div>
-                        <div class="card-event__actions">
-                            <span class="card-event__price"><?php echo $value['preco']; ?></span>
-                            <a href="pacote?id=<?php echo $value['id']; ?>" class="card-event__cart">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </a>
+                    <div class="col-md-4">
+                        <div class="card-event">
+                            <div class="card-event__content">
+                                <figure class="card-event__thumbnail">
+                                    <img src="<?php echo getAbsoluteUrl($value['imagem']); ?>" alt="Rio de Janeiro">
+                                    <span class="card-event__date">
+                                        <?php echo $value['duracao']; ?>
+                                    </span>
+                                </figure>
+                                <span class="card-event__category"><?php echo $value['category']; ?></span>
+                                <h2 class="card-event__title"><?php echo $value['nome']; ?></h2>
+                                <p class="card-event__resume"><?php echo $value['descricao']; ?></p>
+                            </div>
+                            <div class="card-event__actions">
+                                <span class="card-event__price"><?php echo $value['preco']; ?></span>
+                                <a href="pacote?id=<?php echo $value['id']; ?>" class="card-event__cart">
+                                    <i class="fa-solid fa-basket-shopping"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 <?php endforeach; ?>
