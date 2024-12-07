158830a43deade8af76770c3575a0b82260f4903
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-12-07 15:57:07 -0300

    fix: removendo pasta dist

--- dist/js/checkout.js
+++
@@ -1,3 +0,0 @@
-function adicionarAoCarrinho() {
-    window.location.href = 'http://localhost:3000/meu-carrinho';
-}
--- dist/js/home.repository.js
+++
@@ -1,9 +0,0 @@
-const urlApi = 'http://localhost:8000';
-
-function buscarPacotes() {
-    return axios.get(`${urlApi}/pacotes/findAll`, {
-        headers: {
-            'Authorization': localStorage.getItem('token')
-        },
-    })
-}
